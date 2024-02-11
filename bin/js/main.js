3e6137de06a0b9bccf32f4b5fd709a2091884e40
Evan <lintonevan@gmail.com>
2024-02-11 15:13:49 -0400

    update

---
+++ bin/js/main.js
@@ -0,0 +1,14 @@
+function getAge() {
+    const birthDate = new Date('2004-08-31T04:26:00');
+    const currentDate = new Date();
+    currentDate.setUTCHours(0, 0, 0, 0);
+    const timeDiff = currentDate.getTime() - birthDate.getTime();
+    const years = timeDiff / (1000 * 60 * 60 * 24 * 365.25);
+
+    return { age: Math.floor(years), birthdate: birthDate };
+}
+
+window.onload = () => {
+    console.log(`I am ${getAge().age} and was born at approximately ${getAge().birthdate}`);
+    document.getElementById('e-age').innerText = getAge().age;
+}
